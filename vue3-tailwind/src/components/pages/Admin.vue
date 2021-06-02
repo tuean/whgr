589af5a9485f2d81152685c46832c715e6dc45e3
tuean <tuean_z@163.com>
2021-06-02 21:41:19 +0800

    last

--- vue3-tailwind/src/components/pages/Admin.vue
+++ vue3-tailwind/src/components/pages/Admin.vue
@@ -1,13 +1,23 @@
 <template>
-    
+    <div class="bg-default mx-auto h-full w-full">
+       
+    </div>
 </template>
 
 <script>
+import Loading from '/@/components/Loading.vue'
+import { menus } from '/@/conf/menu.js' 
+import '../../index.css'
+
 export default {
+  components: { Loading },
+    setup() {
         
+        return {}
+    }
 }
 </script>
 
-<style scoped>
+<style>
 
-</style>>
+</style>
---
+++ vue3-tailwind/src/conf/menu.js
@@ -0,0 +1,10 @@
+export const menus = [
+    {
+        name: "首页",
+        path: "/admin"
+    }, 
+    {
+        name: "menu1",
+        path: "/menu"
+    }
+]
